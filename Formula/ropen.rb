18d27ebb4b5ed1573925b563ed22c137e6823242
guangyusong <15316444+guangyusong@users.noreply.github.com>
2026-05-03 17:59:01 -0400

    Bump ropen to 0.1.1

--- Formula/ropen.rb
+++ Formula/ropen.rb
@@ -1,8 +1,8 @@
 class Ropen < Formula
   desc "Open remote terminal paths locally on macOS"
   homepage "https://github.com/guangyusong/ropen"
-  url "https://github.com/guangyusong/ropen/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2f4447499e328dc284515fe5a0b9abf5631a7138f72a377c20dbe06f49d83b59"
+  url "https://github.com/guangyusong/ropen/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "ee288ad325fc131d0a1413f2e28a5343a9cb3ed5023f7c65d354cf5d794d3786"
   license "MIT"
 
   depends_on "go" => :build
